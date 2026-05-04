a38fcffe2e388077a35a3cbb5d40a27e23e51a9e
AakashJha04 <aakashthedeveloper04@gmail.com>
2026-05-04 22:59:33 +0530

    Time: 1 ms (100.00%) | Memory: 71.1 MB (71.97%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,47 +1,47 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
         boolean containsOne = false;
+        int n = nums.length;
 
-        // check if one is present in the array if it is then return it.
-        for(int i=0;i<n;i++){
-            if(nums[i]==1)containsOne = true;
+        for(int num:nums){
+            if(num==1){
+                containsOne = true;
+                break;
+            }
         }
 
-        if(containsOne==false)return 1;
+        if(containsOne == false){
+            return 1;
+        }
 
-        // if any element is greater than n or less than 1 then update it with 1.
         for(int i=0;i<n;i++){
-            if(nums[i]>n || nums[i]<=0){
+            if(nums[i]<=0){
+                nums[i]=1;
+            }
+            if(nums[i]>n){
                 nums[i]=1;
             }
         }
 
-        // Apply logic and update the final nums array
+
         for(int i=0;i<n;i++){
-            int number = Math.abs(nums[i]);
-            int idx = number - 1;
-            if(nums[idx]>0){
-                nums[idx] = -1 * nums[idx];
+            int num = Math.abs(nums[i]);
+            int idx = num - 1;
+
+            if(nums[idx]<0){
+                continue;
             }
+
+            nums[idx] = nums[idx] * -1;
         }
 
-        // return the smallest number
         for(int i=0;i<n;i++){
             if(nums[i]>0){
                 return i+1;
             }
         }
 
-        // return n+1.
         return n+1;
-
-
-
-
-
-
-
 
 
     }
