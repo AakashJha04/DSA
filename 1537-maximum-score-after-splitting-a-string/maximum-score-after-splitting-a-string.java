f8e58d65e288a9394943776209e86ff0771d8874
AakashJha04 <aakashthedeveloper04@gmail.com>
2026-05-31 10:53:46 +0530

    Time: 1 ms (99.67%) | Memory: 42.8 MB (50.43%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,25 +1,24 @@
 class Solution {
     public int maxScore(String s) {
+        int ones = 0;
+
+        for(char ch : s.toCharArray()) {
+            if(ch == '1') {
+                ones++;
+            }
+        }
+
+        int zeros = 0;
         int ans = 0;
-        int n = s.length();
 
-        for(int i = 0; i < n - 1; i++) {
-            int leftScore = 0;
-            int rightScore = 0;
-
-            for(int j = 0; j <= i; j++) {
-                if(s.charAt(j) == '0') {
-                    leftScore++;
-                }
+        for(int i = 0; i < s.length() - 1; i++) {
+            if(s.charAt(i) == '0') {
+                zeros++;
+            } else {
+                ones--;
             }
 
-            for(int j = i + 1; j < n; j++) {
-                if(s.charAt(j) == '1') {
-                    rightScore++;
-                }
-            }
-
-            ans = Math.max(ans, leftScore + rightScore);
+            ans = Math.max(ans, zeros + ones);
         }
 
         return ans;
