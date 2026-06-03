b74820b7b72c2174fe31d89c3527834e79f6e7a2
AakashJha04 <aakashthedeveloper04@gmail.com>
2026-06-03 08:37:40 +0530

    Time: 8 ms (90.45%) | Memory: 48.8 MB (90.22%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,29 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int[][] merge(int[][] intervals) {
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
+        List<int[]> ans = new ArrayList<>();
 
-        List<int[]> merged = new ArrayList<>();
-
-        int start = intervals[0][0];
-        int end = intervals[0][1];
+        int start = intervals[0][0]; //1
+        int end = intervals[0][1]; //3
 
         for (int i = 1; i < intervals.length; i++) {
-
-            if (intervals[i][0] <= end) {
-                end = Math.max(end, intervals[i][1]);
+            if (intervals[i][0] <= end) { //2<=3
+                end = Math.max(end, intervals[i][1]); //3, 6 
             } else {
-                merged.add(new int[]{start, end});
-
+                ans.add(new int[] { start, end });
                 start = intervals[i][0];
                 end = intervals[i][1];
+
             }
         }
 
-        merged.add(new int[]{start, end});
+        ans.add(new int[] { start, end });
+        return ans.toArray(new int[ans.size()][]);
 
-        return merged.toArray(new int[merged.size()][]);
     }
 }
